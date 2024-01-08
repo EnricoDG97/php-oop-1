3ae33370a578a23869af75779885b25c9a0d475f
EnricoDG97 <enrico.delgaudio97@gmail.com>
2024-01-08 23:42:49 +0100

    feat: added two movies constructed in array, foreach movie echo in html

--- index.php
+++ index.php
@@ -2,4 +2,25 @@
 
 require_once __DIR__ . "/Models/Movie.php";
 
+$movies = [
+    new Movie("The Matrix", "Action", "1990"),
+    new Movie("Forrest Gump", "Tragicomic", "1992")
+];
+
+var_dump($movies);
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies List</title>
+</head>
+<body>
+    <?php foreach ($movies as $movie) { 
+        echo '<p>' . $movie->getMovieDetails() . '</p>';
+    }?>
+</body>
+</html>
