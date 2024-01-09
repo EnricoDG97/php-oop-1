09bd0f88107b2a22605d6bc2f7a306fa42c80209
EnricoDG97 <enrico.delgaudio97@gmail.com>
2024-01-09 10:39:23 +0100

    feat: added database section contains data arrays

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
-
-require_once __DIR__ . "/Models/Movie.php";
-require_once __DIR__ . "/Models/Genre.php";
-
-$action = new Genre("Action");
-$comedy = new Genre("Comody");
-$tragicomic = new Genre("Tragicomic");
-
-
-$movies = [
-    new Movie("The Matrix", [$action, $comedy], "1990"),
-    new Movie("Forrest Gump", [$action, $tragicomic], "1992")
-];
-
-var_dump($movies);
-
+require_once __DIR__ . '/database/db.php';
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
