bb14f67d4f746fba21383e7b64e3224cb64f812e
EnricoDG97 <enrico.delgaudio97@gmail.com>
2024-01-09 10:19:21 +0100

    feat: added new Genre Class in index and Modules,  modified GetMovieDetails function

--- Models/Movie.php
+++ Models/Movie.php
@@ -3,18 +3,27 @@
 class Movie
 {
     public $title;
-    public $genre;
+    // genres sarà un array e non più una stringa unica
+    public $genres;
     public $year;
 
-    public function __construct($_title, $_genre, $_year)
+    public function __construct($_title, $_genres, $_year)
     {
         $this->title = $_title;
-        $this->genre = $_genre;
+        $this->genres = $_genres;
         $this->year = $_year;
     }
 
     public function getMovieDetails()
     {
-        return "Title: $this->title, Genre: $this->genre, Year: $this->year";
+        // trasformo l'array in stringa, lo popolo aggiungendo le , concatenate
+        $genres_string = "";
+        foreach($this->genres as $key => $genre) {
+            $genres_string .= $genre->name;
+            if($key < count($this->genres) - 1) {
+                $genres_string .= ', ';
+            }
+        }
+        return "Title: $this->title, Genre: $genres_string, Year: $this->year";
     }
 }
--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 
 require_once __DIR__ . "/Models/Movie.php";
+require_once __DIR__ . "/Models/Genre.php";
+
+$action = new Genre("Action");
+$comedy = new Genre("Comody");
+$tragicomic = new Genre("Tragicomic");
+
 
 $movies = [
-    new Movie("The Matrix", "Action", "1990"),
-    new Movie("Forrest Gump", "Tragicomic", "1992")
+    new Movie("The Matrix", [$action, $comedy], "1990"),
+    new Movie("Forrest Gump", [$action, $tragicomic], "1992")
 ];
 
 var_dump($movies);
